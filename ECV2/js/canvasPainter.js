707fb36935081ff427338e2f7a90b2ba405fb4a9
abeldella <abel.della@gmail.com>
2017-01-26 15:52:50 +0100

    Start assign 2

---
+++ ECV2/js/canvasPainter.js
@@ -0,0 +1,69 @@
+function CanvasPainter( canvas ){
+	this.canvas = document.quierySelector( canvas_selector );
+	this.canvas.style.backgroundColor = "black"
+	this.draw();
+	this.drawing = false;
+	
+	var that = this;
+	
+	function loop(){
+		that.draw();
+		requireAnimationFrame( loop );
+	}
+	
+	loop();
+}
+
+CanvasPainter.prototype.animate = function(){
+	var that = this;
+	function loop(){
+		that.draw();
+		requestAnimationFrame(loop);
+	}
+}
+
+CanvasPainter.prototype.bindEvents = frunction(){
+	this.canvas.addEventListener("mousedown",this.onMouseEvent.bind( this.));
+	this.canvas.addEventListener("mousemove",this.onMouseEvent.bind(this));
+	this.canvas.addEventListener("mouseup",this.onMouseEvent.bind(this));
+}
+
+CanvasP.prototype.onMouseEvent = function (e){
+	console.log(e.client);
+	
+	if(e.type == "mousemove"){
+		
+	}
+	else if(e.type == "mousemove"){
+		this.drawing = 
+	}
+	else if(e.type == "mouseup"){
+	
+	}
+}
+
+CanvasPrototype.prototype.executeAction = function(){
+	if(action = "paint"){
+		var x = 0;
+		var y = 0;
+		this.ctx.fillStyle = "green";
+		this.ctx.fillRect(x,y,10,10);
+	}
+	if(action = "circle"){
+		var x = 0;
+		var y = 0;
+		this.ctx.fillStyle = "green";
+		this.ctx.fillCirc(x,y,10,10);
+	}
+}
+
+CanvasPAinter.prototype.draw = function(){
+	var ctx = this.canvas.getContext("2d");
+	
+	ctx.fillStyle = "red";
+	ctx.fillRect(0,0, this.canvas.width, this.canvas.height);
+	
+	ctx.strokeStyle = "white";
+	
+	ctx.strokeRect(Math.sin(performance.now()*0.001)*100+200,0,100,100);
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Abel Dellà Morte
+	NIA: 164762
+	Correo: abel.della@gmail.com
+
+Sebastián Maya Hernández
+	NIA: 163607
+	Correo: sebastiancamilo.maya01@estudiant.upf.edu
